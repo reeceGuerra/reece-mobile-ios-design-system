bb5ef44fe57f6c7eabb21f53c1a7fc0880288d34
Carlos Guerra <carlos.guerra@morsco.com>
2025-08-30 21:13:53 -0600

    [BUILD]: add iOS demo app scaffold under Examples/

---
+++ Examples/ReeceDesignSystemDemo/ReeceDesignSystemDemo/ReeceDesignSystemDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ReeceDesignSystemDemo
+//
+//  Created by Carlos Lopez on 30/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Examples/ReeceDesignSystemDemo/ReeceDesignSystemDemo/ReeceDesignSystemDemo/ReeceDesignSystemDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReeceDesignSystemDemoApp.swift
+//  ReeceDesignSystemDemo
+//
+//  Created by Carlos Lopez on 30/08/25.
+//
+
+import SwiftUI
+
+@main
+struct ReeceDesignSystemDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
